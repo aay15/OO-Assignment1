82f08b97f5bc81ae9e5ef4c4fbb2ca1a25d2b10c
Andrew Rohn <atrohn@gmail.com>
2015-07-09 22:14:45 -0500

    Removed bug because the Inventory was not instantiated in VideoStore

--- VideoStore.java
+++ VideoStore.java
@@ -4,15 +4,14 @@ public class VideoStore {
     
     static Scanner sc = new Scanner(System.in);
     
-    private Inventory storeInventory;
+    private Inventory storeInventory = new Inventory();
 
     public static void main(String[] args) {    
         VideoStore store = new VideoStore();
-        displayMenu();
         store.enterChoice();
     }
 
-    static void displayMenu() {
+    private void displayMenu() {
         System.out.println("Video Store Inventory Menu");
         System.out.println("1. Add Movie");
         System.out.println("2. Remove Movie");
@@ -21,25 +20,30 @@ public class VideoStore {
         System.out.println("5. Quit the Program");
     }
       
-    void enterChoice() {
+    private void enterChoice() {
+        displayMenu();
         System.out.println("Enter your choice: ");
         int choice = sc.nextInt();
 
         switch (choice) {
             case 1: //Add movie     
                 storeInventory.addMovie(createNewMovie());
+                enterChoice();
             break;
 
             case 2: //Remove movie
                 storeInventory.removeMovieBySku(requestSku());
+                enterChoice();
             break;
 
             case 3: //Display movie
                 storeInventory.displayMovieBySku(requestSku());
+                enterChoice();
             break;
 
             case 4: //Display inventory
-            
+                
+                enterChoice();
             break;
 
             case 5: //Quit program
